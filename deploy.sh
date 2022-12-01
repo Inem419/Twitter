45dfcc3acfb5c970e736d246d6de34820a2f4d7d
Inem Udowana <inem404@gmail.com>
2022-11-30 19:50:31 -0500

    Updated deployment and monitors

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ Deploy python apps
 Deploy in the dev env
 deployment5 master
 deployment4
+deployment4
 deploy to staging environment
 echo good job
 Testd and accepted
--- monitor.app
+++ monitor.app
@@ -2,4 +2,3 @@ grafana
 newrelic
 prometheus
 appDynamics
-dynatrace
